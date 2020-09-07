700dd5e7908a472fe4c7e526046ce3219be8fe4f
sachin <sachinbansal789@gmail.com>
2020-09-07 22:51:38 +0530

    ADDED:valid last name

--- userregistraion.sh
+++ userregistraion.sh
@@ -10,3 +10,15 @@ then
 else
 	echo "first name is invalid";
 fi
+
+#UseCase 2 => check valid last name
+
+read -p "enter last name:" lastName
+pattern=^[A-Z]{1}[a-z]{2}
+
+if [[ $lastName =~ $pattern ]]
+then
+	echo "last name is valid";
+else
+	echo "last name is invalid";
+fi
